71ea300048ac80bc829d9c6457808053cba829a7
constroy <constroy.li@gmail.com>
2026-06-23 23:37:40 +0800

    complete 02-vector-add

--- puzzles/02-vector-add.py
+++ puzzles/02-vector-add.py
@@ -106,6 +106,11 @@ def tl_mul_relu_1d(A, B, BLOCK_N: int):
     C = T.empty((N,), T.float16)
 
     # TODO: Implement this function
+    with T.Kernel(T.ceildiv(N, BLOCK_N), threads=256) as bx:
+        base_idx = bx * BLOCK_N
+        for i in T.Parallel(BLOCK_N):
+            c = A[base_idx + i] * B[base_idx + i]
+            C[base_idx + i] = T.if_then_else(c > 0, c, 0)
 
     return C
 
@@ -169,6 +174,17 @@ def tl_mul_relu_1d_mem(A, B, BLOCK_N: int):
     C = T.empty((N,), dtype)
 
     # TODO: Implement this function
+    with T.Kernel(T.ceildiv(N, BLOCK_N), threads=256) as bx:
+        base_idx = bx * BLOCK_N
+        A_local = T.alloc_fragment((BLOCK_N,), dtype)
+        B_local = T.alloc_fragment((BLOCK_N,), dtype)
+        C_local = T.alloc_fragment((BLOCK_N,), dtype)
+        T.copy(A[base_idx], A_local)
+        T.copy(B[base_idx], B_local)
+        for i in T.Parallel(BLOCK_N):
+            c = A_local[i] * B_local[i]
+            C_local[i] = T.if_then_else(c > 0, c, 0)
+        T.copy(C_local, C[base_idx])
 
     return C
 
